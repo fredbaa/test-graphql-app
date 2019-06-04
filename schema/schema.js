00cf2bcc7f77ce271623a6070a6014ae8ba4e4bf
Fred Baa <frederickbaa@gmail.com>
2019-06-04 12:59:24 +0800

    added companytype and resolver for user query

--- schema/schema.js
+++ schema/schema.js
@@ -6,16 +6,34 @@ const dbUrl = "http://localhost:3000";
 const {
   GraphQLObjectType,
   GraphQLString,
+  GraphQLID,
   GraphQLInt,
   GraphQLSchema
 } = graphql;
 
+const CompanyType = new GraphQLObjectType({
+  name: 'Company',
+  fields: {
+    id: { type: GraphQLID },
+    name: { type: GraphQLString },
+    description: { type: GraphQLString }
+  }
+});
+
 const UserType = new GraphQLObjectType({
   name: 'User',
   fields: {
-    id: { type: GraphQLString },
+    id: { type: GraphQLID },
     firstName: { type: GraphQLString },
-    age: { type: GraphQLInt }
+    age: { type: GraphQLInt },
+    company: {
+      type: CompanyType,
+      resolve(parentValue, args) {
+        // parentValue is the user value data (direct parent of this resolve)
+        return axios.get(`${dbUrl}/companies/${parentValue.companyId}`)
+                    .then(res => res.data)
+      }
+    }
   }
 });
 
@@ -24,7 +42,7 @@ const RootQuery = new GraphQLObjectType({
   fields: {
     user: {
       type: UserType,
-      args: { id: { type: GraphQLString } },
+      args: { id: { type: GraphQLID } },
       resolve(parentValue, args) {
         return axios.get(`${dbUrl}/users/${args.id}`)
                     .then(resp => resp.data); // needs this because of axios returns a 'data' column
